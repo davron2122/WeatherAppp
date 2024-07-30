f85dd0cfbba6b9e249f02bf7293fd64e7c900ebf
Davron <eliboyevdavron2122@gmail.com>
2024-07-30 14:10:44 +0900

    make Main UI xml

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -12,13 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        //biz negadir hakka narsaga oson erushushga irganub  qolhanmiz b bega fuit aybni bishqadaan axrtaramiz
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 
             insets
+
             //
         }
     }
