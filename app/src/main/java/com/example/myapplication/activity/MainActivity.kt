71bc0a95e4e608026b98c59f76cb9eb37bfd79f4
Davron <eliboyevdavron2122@gmail.com>
2024-07-31 09:47:41 +0900

    CallCurrentApi retrofit in MainActivity

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -1,26 +1,140 @@
 package com.example.myapplication.activity
 
+import android.graphics.Color
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.model.CurrentResponseApi
+import com.example.myapplication.viewModel.WeatherViewModel
+import com.github.matteobattilana.weather.PrecipType
+import retrofit2.Call
+import retrofit2.Response
+
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val weatherViewModel: WeatherViewModel by viewModels()
+
+    private val calendar by lazy { Calendar.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-
-            insets
-
-            //
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = Color.TRANSPARENT
         }
+
+        binding.apply {
+            var lat = 51.50
+            var lan = -0.12
+            var name = "London"
+
+
+            cityTxt.text = name
+            progressBar.visibility = View.VISIBLE
+            weatherViewModel.loadCurrentWeather(lat, lan, "metric").enqueue(object :
+                retrofit2.Callback<CurrentResponseApi> {
+                override fun onResponse(
+                    call: Call<CurrentResponseApi>,
+                    response: Response<CurrentResponseApi>
+                ) {
+                    if (response.isSuccessful) {
+                        val data = response.body()
+                        progressBar.visibility = View.GONE
+                        detailedlayout.visibility = View.VISIBLE
+                        data?.let {
+                            statusTxt.text = it.weather?.get(0)?.main ?: "-"
+                            windTxt.text = it.wind?.speed.let { Math.round(it!!).toString() } + "KM"
+                            currentTempTxt.text =
+                                it.main?.temp.let { Math.round(it!!).toString() } + "°"
+                            maxTempTxt.text =
+                                it.main?.tempMax.let { Math.round(it!!).toString() } + "°"
+                            minTempTxt.text =
+                                it.main?.tempMin.let { Math.round(it!!).toString() } + "°"
+
+
+                            val drawable = if (isNightNow()) R.drawable.night_bg
+                            else {
+                              setDynamicallyWallpaper(it.weather?.get(0)?.icon?:"-")
+                            }
+                            bgImage.set
+
+                        }
+
+                    }
+                }
+
+                override fun onFailure(call: Call<CurrentResponseApi>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_SHORT).show()
+
+                }
+
+            })
+
+        }
+
     }
+
+    private fun isNightNow(): Boolean {
+        return calendar.get(Calendar.HOUR_OF_DAY) >= 18
+    }
+    private fun setDynamicallyWallpaper(icon:String):Int{
+        return when (icon.dropLast(1)) {
+            "01" -> {
+                initWeatherView(PrecipType.CLEAR)
+                R.drawable.snow_bg
+            }
+
+            "02", "03", "04"-> {
+                initWeatherView(PrecipType.CLEAR)
+                R.drawable.cloudy_bg
+
+            }
+
+            "09", "10", "11"  -> {
+                initWeatherView(PrecipType.RAIN)
+                R.drawable.rainy_bg
+            }
+
+            "13" -> {
+                initWeatherView(PrecipType.SNOW)
+                R.drawable.snow_bg
+            }
+
+            "50" -> {
+                initWeatherView(PrecipType.CLEAR)
+                R.drawable.snow_bg
+            }
+            else -> 0
+        }
+
+        }
+        private fun initWeatherView (type:PrecipType){
+            binding.weatherView.apply {
+                setWeatherData(type)
+                angle = 20
+                emissionRate = 100.0f
+            }
+
+
+
+    }
+
+
 }
