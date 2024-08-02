a238546f7f8aab69353781374c507892fead7e52
Davron <eliboyevdavron2122@gmail.com>
2024-08-02 17:14:13 +0900

    added google-services.json, fixed firebase errors

--- app/src/main/java/com/example/myapplication/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/myapplication/firebase/MyFirebaseMessagingService.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
 
 
 class MyFirebaseMessagingService: FirebaseMessagingService(){
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-    }
+
+
+
 
 }
